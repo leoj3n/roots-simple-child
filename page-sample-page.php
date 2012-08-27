ed2aade4aa19042e48bc4227d904f82dc0e9d012
Joel Kuczmarski <leoj3n@gmail.com>
2012-08-27 07:48:44 -0500

    Added comment to example page template

--- page-sample-page.php
+++ page-sample-page.php
@@ -1,3 +1,5 @@
+// This template will be used if the page slug is "sample-page"
+
 <?php get_template_part('templates/page', 'header'); ?>
 
 <p>This page content is brought to you by <code>page-sample-page.php</code> in roots-simple-child...</p>
